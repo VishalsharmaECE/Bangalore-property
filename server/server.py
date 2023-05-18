886eec663a36765eeda246a75b73126ba26be47c
VishalsharmaECE <vs2418@ece.jgec.ac.in>
2023-05-18 15:07:39 +0530

    xyz4

--- server/server.py
+++ server/server.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 @app.route('/get_location_names', methods = ['GET'])
 def get_location_names():
+    util.load_saved_artifacts()
     response = jsonify({
         'locations': util.get_location_names()
     })
